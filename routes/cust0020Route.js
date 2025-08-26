e5fada0f7b72f5f7ef811ec6a011beb2408a853c
woalstm85 <zzams1985@gmail.com>
2025-08-26 10:27:22 +0900

    newone server

--- routes/cust0020Route.js
+++ routes/cust0020Route.js
@@ -13,6 +13,5 @@ const cust0020Handler = createProcedureExecutor(
 
 // 대문자와 소문자 둘 다 지원
 router.post('/api/CUST0020', cust0020Handler);
-router.post('/api/cust0020', cust0020Handler);
 
 module.exports = router;
